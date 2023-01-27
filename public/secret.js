49863cd27a5341d8d621c4432ecfd08806ea08d4
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-01-27 23:42:20 +0600

    complete payment

---
+++ public/secret.js
@@ -0,0 +1,11 @@
+// REACT_APP_apiKey=AIzaSyAIeuLfpKd8MTMzaYygboDtuneli_P7eVs
+// REACT_APP_authDomain=doctor-chamber-3421f.firebaseapp.com
+// REACT_APP_projectId=doctor-chamber-3421f
+// REACT_APP_storageBucket=doctor-chamber-3421f.appspot.com
+// REACT_APP_messagingSenderId=323483176564
+// REACT_APP_appId=1:323483176564:web:f9ced972c471df86f30b21
+
+
+// REACT_APP_imgbb_key=19c98c7dd515d8a39a34736adef23d64
+
+// REACT_APP_STRIPE_PK=pk_test_51MUtJ3HKWwYR1p914MU3zAdBG7ZbDXCCIziUD4iqEbbVPkt8lxTxQ5CN2ayx2QHPsbCQwj9vkh8BoniQzmLN4A6V003i593gT1
